5289dec59dc9eebb87935b3f2457ed2bf505ae1b
Luiz Felipe <luckpecontato@gmail.com>
2022-12-03 17:54:09 -0300

    feat: rendering random words when entering the page

--- script.js
+++ script.js
@@ -4,82 +4,6 @@ let correctTyped = 0;
 let wordTyped = 0;
 const timerCountElement = document.querySelector(".displayTimer");
 
-
-const words = [
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-  ["l", "u", "i", "z"],
-  ["a", "n", "d", "r", "e"],
-  ["i", "s"],
-  ["a", "w", "e", "s", "o", "m", "e"],
-  ["a", "n", "d"],
-  ["a", "l", "s", "o"],
-];
-
 const renderWord = (word) => {
   const wordElement = document.createElement("div");
   wordElement.classList.add("word");
@@ -92,7 +16,11 @@ const renderWord = (word) => {
 };
 
 const renderWords = (words) => {
-  words.forEach((word) => renderWord(word));
+  typebox.innerHTML = "";
+  //renderizar 100 palavras aleatorias
+  for (let i = 0; i < 100; i++) {
+    renderWord(words[Math.floor(Math.random() * words.length)]);
+  }
 };
 
 renderWords(words); // render first page
@@ -122,8 +50,8 @@ const checkTypeBackspace = (letter, letterElements) => {
 const typeLetter = (letter) => {
   const testLength = letterTyped >= 0;
   if (testLength) {
+    checkTypeBackspace(letter, letterElements);
     if (letterTyped === letterElements.length) {
-      checkTypeBackspace(letter, letterElements);
       if (letter === " ") {
         wordElements[wordTyped].remove();
         wordTyped++;
@@ -134,11 +62,11 @@ const typeLetter = (letter) => {
     }
     const elementLetter = letterElements[letterTyped].textContent;
     checkCorrectTyped(letter, elementLetter, letterElements);
-    checkTypeBackspace(letter, letterElements);
   }
 };
 
 addEventListener("keydown", (event) => {
+  console.log(event);
   if (letterTyped == 0 && wordTyped == 0){
     startTimer(30, timerCountElement)
   }
@@ -156,7 +84,7 @@ function startTimer(duration, display) {
     display.textContent = seconds;
     if (--timer < 0) {
       timer = duration;
-      display.textContent = correctTyped/5/(duration/60)
+      display.textContent = correctTyped/5/(duration/60) + "WPM"
       clearInterval(interval);
     }
   }, 1000);
